25c3af16d1c1e310af71a15e6da2c755eb451c9f
Minoo Kim <minookim.k@gmail.com>
2022-01-30 17:42:15 -0800

    Add files via upload

---
+++ student.java
@@ -0,0 +1,41 @@
+import java.util.*;
+public class student {
+    // stores json ids 
+    static ArrayList<Integer> taken;
+    static ArrayList<Integer> gr9;
+    static ArrayList<Integer> gr10;
+    static ArrayList<Integer> gr11;
+    static ArrayList<Integer> gr12;
+
+    public static void main(String[] args){
+        System.out.println("testing java");
+    }
+
+    static boolean addClass(int cl){
+        if(taken.contains(cl)){
+            taken.add(cl);
+        }
+        else{
+            return false;
+        }
+        return true;
+    }
+    
+    static class Course {
+		String name;
+        String prereq;
+        int hw;
+        String level;
+        String category;
+
+        public Course(String name, String prereq, int hw, String level, String category){
+            this.name=name;
+            this.prereq=prereq;
+            this.hw=hw;
+            this.level=level;
+            this.category=category;
+        }
+	} 
+}
+
+
